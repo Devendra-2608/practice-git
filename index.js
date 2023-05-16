c89d480d5507043c51fe58354a9b75dbe5b4df34
nikunj ladva crest <nikunj.l@crestinfosystems.com>
2023-05-16 12:45:56 +0530

    add /nik

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get("/", (req, res) => {
   res.send("welcome to git revise");
 });
 
+
+app.get("/nik", (req, res) => {
+  res.send("welcome to git revise..mabn");
+});
+
+
 app.listen(3999, () => {
   console.log("server run on 3030");
 });
